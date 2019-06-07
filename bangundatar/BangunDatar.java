8bd35ec5e1f99a523c7695a1fc88af23b86a4cf4
basri27 <51488169+basri27@users.noreply.github.com>
2019-06-08 00:03:40 +0800

    BangunDatar.java

--- bangundatar/BangunDatar.java
+++ bangundatar/BangunDatar.java
@@ -4,3 +4,14 @@ public interface BangunDatar {
     public double luas(int s1, int s2);
     public double keliling(int s1, int s2);
 }
+
+public main implements BangunDatar(){
+    public static void main(String[]args){
+       public double luas(int p, int l){
+          int luas = p*l;
+       }
+       public double keliling(int p, int l){
+          int keliling = 2*(p+l);
+       }
+    }
+}
